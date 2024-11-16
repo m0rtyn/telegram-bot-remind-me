73807e1870ac40ca3c5c4e5d079b57331e44723d
Martyn <zogacc@gmail.com>
2024-11-16 16:21:34 +0100

    fix: env variable names

--- src/constants/dev.ts
+++ src/constants/dev.ts
@@ -1,10 +1,10 @@
 import { TELEGRAM_BOT_URL } from "./bot.js"
 
-export const RENDER_URL = process.env.RENDER_URL
+export const WEB_URL = process.env.HOSTING_URL
 export const NGROK_URL = process.env.NGROK_URL
 
 // export const WEB_APP_URL = process.env.RENDER_URL
 export const WEB_APP_URL =
-  process.env.NODE_ENV === "development" ? NGROK_URL : RENDER_URL
+  process.env.NODE_ENV === "development" ? NGROK_URL : WEB_URL
 
 export const WEBHOOK_URL = TELEGRAM_BOT_URL + "/setWebhook?url=" + WEB_APP_URL
